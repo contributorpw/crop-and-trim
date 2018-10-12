b962c276feaa4a7fd460fbfa7a0ecb8fb1c81fa4
Alexander Ivanov <oshli.a.er@gmail.com>
2018-10-12 17:03:30 +0300

    Add batch operations

--- src/code.js
+++ src/code.js
@@ -6,7 +6,9 @@
  */
 
 // ESLint config.
-/* exported onOpen, onInstall, cropToSelection, cropToData */
+/* exported onOpen, onInstall */
+/* exported cropToSelection, cropToData */
+/* exported cropAllToData, cropAllToCornerSelection */
 
 /**
  * Adds a menu when the spreadsheet is opened.
@@ -15,6 +17,8 @@ function onOpen() {
   SpreadsheetApp.getUi().createAddonMenu()
       .addItem('Crop to data', 'cropToData')
       .addItem('Crop to selection', 'cropToSelection')
+      .addItem('Crop all to data', 'cropAllToData')
+      .addItem('Crop all to corner of selection', 'cropAllToCornerSelection')
       .addToUi();
 }
 
@@ -32,6 +36,9 @@ function cropToSelection() {
   console.log('Cropping to selection');
   var range = SpreadsheetApp.getActiveSheet().getActiveRange();
   cropToRange(range);
+  range.getSheet()
+      .getRange(1, 1, sheet.getMaxRows(), sheet.getMaxColumns()).activate();
+  showCompleteMessage(range.getSheet().getParent());
 }
 
 /**
@@ -41,6 +48,44 @@ function cropToData() {
   console.log('Cropping to data');
   var range = SpreadsheetApp.getActiveSheet().getDataRange();
   cropToRange(range);
+  range.getSheet()
+      .getRange(1, 1, sheet.getMaxRows(), sheet.getMaxColumns()).activate();
+  showCompleteMessage(range.getSheet().getParent());
+}
+
+/**
+ * Crops all sheets to the data.
+ */
+function cropAllToData() {
+  console.log('Cropping to data all sheets');
+  var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getSheets().forEach(function(sheet) {
+    var range = sheet.getDataRange().activate();
+    Utilities.sleep(1000);
+    cropToRange(range);
+  });
+  showCompleteMessage(spreadsheet);
+}
+
+/**
+ * Trim all sheets to bottom and right based on the current active cell
+ */
+function cropAllToCornerSelection() {
+  console.log('Cropping to corner of selection all sheets');
+  var spreadsheet = SpreadsheetApp.getActive();
+  var activeRange = spreadsheet.getActiveRange();
+  var lastRow_ = activeRange.getLastRow();
+  var lastColumn_ = activeRange.getLastColumn();
+  spreadsheet.getSheets().forEach(function(sheet) {
+    var maxRows_ = sheet.getMaxRows();
+    var maxColumns_ = sheet.getMaxColumns();
+    var numRows_ = lastRow_ > maxRows_ ? maxRows_ : lastRow_;
+    var numColumns_ = lastColumn_ > maxColumns_ ? maxColumns_ : lastColumn_;
+    var range = sheet.getRange(1, 1, numRows_, numColumns_).activate();
+    Utilities.sleep(1000);
+    cropToRange(range);
+  });
+  showCompleteMessage(spreadsheet);
 }
 
 /**
@@ -49,7 +94,6 @@ function cropToData() {
  */
 function cropToRange(range) {
   var sheet = range.getSheet();
-  var spreadsheet = sheet.getParent();
   var firstRow = range.getRow();
   var lastRow = firstRow + range.getNumRows() - 1;
   var firstColumn = range.getColumn();
@@ -69,8 +113,6 @@ function cropToRange(range) {
   if (firstColumn > 1) {
     sheet.deleteColumns(1, firstColumn - 1);
   }
-  sheet.getRange(1, 1, sheet.getMaxRows(), sheet.getMaxColumns()).activate();
-  showCompleteMessage(spreadsheet);
 }
 
 /**
